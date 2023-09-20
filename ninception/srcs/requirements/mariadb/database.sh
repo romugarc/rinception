a975c851aa90831371e5048237fec969e59b00fd
Romuald Garcia <rgarcia@z1r2p4.42lyon.fr>
2023-09-20 11:21:48 +0200

    rm mysqladmin

--- ninception/srcs/requirements/mariadb/database.sh
+++ ninception/srcs/requirements/mariadb/database.sh
@@ -4,7 +4,7 @@ mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}' @ '%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}' @ '%' IDENTIFIED BY '${DB_USER_PASSWORD}' WITH GRANT OPTION;"
 mysql -e "FLUSH PRIVILEGES;"
-sed -i		"s/password = /password = ${MARIADB_ROOT_PWD} #/" /etc/mysql/debian.cnf
+sed -i		"s/password = /password = ${DB_ROOT_PASSWORD} #/" /etc/mysql/debian.cnf
 mysql -e "ALTER USER 'root' @ 'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
 service mysql stop
 mysqld_safe
