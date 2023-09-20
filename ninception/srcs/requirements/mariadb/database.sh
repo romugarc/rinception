13738fc1f3f10a565ab92d76e445eb49382428cd
Romuald Garcia <rgarcia@z1r2p4.42lyon.fr>
2023-09-20 15:59:27 +0200

    rm mysqladmin

--- ninception/srcs/requirements/mariadb/database.sh
+++ ninception/srcs/requirements/mariadb/database.sh
@@ -8,8 +8,8 @@ then
 	mysql -e	"CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
 	mysql -e	"GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}' WITH GRANT OPTION;"
 	mysql -e	"FLUSH PRIVILEGES;"
-	sed -i		"s/password = /password = ${DB_ROOT_PWD} #/" /etc/mysql/debian.cnf
-	mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSORWD}'"
+	sed -i		"s/password = /password = ${DB_ROOT_PASSWORD} #/" /etc/mysql/debian.cnf
+	mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'"
 	service		mysql stop
 else
 	echo "No need to create database, the old one was retrieved"
