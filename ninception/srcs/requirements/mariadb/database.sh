965ec2f191ef9d14a8062233723447ec9ed604ff
Romuald Garcia <rgarcia@z1r2p4.42lyon.fr>
2023-09-20 10:57:03 +0200

    rm mysqladmin

--- ninception/srcs/requirements/mariadb/database.sh
+++ ninception/srcs/requirements/mariadb/database.sh
@@ -3,7 +3,7 @@ service mysql start
 mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}' @ '%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}' @ '%' IDENTIFIED BY '${DB_USER_PASSWORD}' WITH GRANT OPTION;"
-mysql -e "ALTER USER 'root' @ 'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
+mysql -e "ALTER USER 'root' @ 'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
 service mysql stop
 mysqld_safe
