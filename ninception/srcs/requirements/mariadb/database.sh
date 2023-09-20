9a936060a888e5d5a7a29a2bddd4c09fa803f762
Romuald Garcia <rgarcia@z1r2p4.42lyon.fr>
2023-09-20 16:45:57 +0200

    big change

--- ninception/srcs/requirements/mariadb/database.sh
+++ ninception/srcs/requirements/mariadb/database.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 # check if the database as been retrieved 
-if [ ! -d "/var/lib/mysql/${DB_NAME}" ]
+if [ ! -d "/var/lib/mysql/${MARIADB_NAME}" ]
 then
+
+	echo "creating a new database"
+	# start mysql
 	service		mysql start
+	# wait for mysql to finish starting
 	sleep 5
-	mysql -e 	"CREATE DATABASE ${DB_NAME};"
-	mysql -e	"CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
-	mysql -e	"GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}' WITH GRANT OPTION;"
+	# create a new database the -e flag simulate opening the database and running the command inside without having to do it
+	mysql -e 	"CREATE DATABASE ${MARIADB_NAME};"
+	# create a user for the database
+	mysql -e	"CREATE USER '${MARIADB_USR}'@'%' IDENTIFIED BY '${MARIADB_USR_PWD}';"
+	# self explanatory
+	mysql -e	"GRANT ALL PRIVILEGES ON ${MARIADB_NAME}.* TO '${MARIADB_USR}'@'%' IDENTIFIED BY '${MARIADB_USR_PWD}' WITH GRANT OPTION;"
+	# update all privileges
 	mysql -e	"FLUSH PRIVILEGES;"
-	sed -i		"s/password = /password = ${DB_ROOT_PASSWORD} #/" /etc/mysql/debian.cnf
-	mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'"
+	# set the root password inside the debian.cnf flag to be able to restart mysql as root
+	sed -i		"s/password = /password = ${MARIADB_ROOT_PWD} #/" /etc/mysql/debian.cnf
+	# prevent root ability to start mysql without entering his password
+	mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}'"
+	# stop mysql in order to restart it the propoer way as said in the official documentation
 	service		mysql stop
 else
 	echo "No need to create database, the old one was retrieved"
