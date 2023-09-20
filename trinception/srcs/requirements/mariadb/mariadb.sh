fa9cf9fe862dc5eb28144e52cc8acae26e7c15c4
Romuald Garcia <rgarcia@z1r2p4.42lyon.fr>
2023-09-20 23:05:09 +0200

    trinception

--- trinception/srcs/requirements/mariadb/mariadb.sh
+++ trinception/srcs/requirements/mariadb/mariadb.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-if [ ! -d "/var/lib/mysql/${MARIADB_NAME}" ]
+if [ ! -d "/var/lib/mysql/${DB_NAME}" ]
 then
 
 	service		mysql start
 	sleep 5
-	mysql -e 	"CREATE DATABASE ${MARIADB_NAME};"
-	mysql -e	"CREATE USER '${MARIADB_USR}'@'%' IDENTIFIED BY '${MARIADB_USR_PWD}';"
-	mysql -e	"GRANT ALL PRIVILEGES ON ${MARIADB_NAME}.* TO '${MARIADB_USR}'@'%' IDENTIFIED BY '${MARIADB_USR_PWD}' WITH GRANT OPTION;"
+	mysql -e 	"CREATE DATABASE ${DB_NAME};"
+	mysql -e	"CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
+	mysql -e	"GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}' WITH GRANT OPTION;"
 	mysql -e	"FLUSH PRIVILEGES;"
-	sed -i		"s/password = /password = ${MARIADB_ROOT_PWD} #/" /etc/mysql/debian.cnf
-	mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}'"
+	sed -i		"s/password = /password = ${DB_USER_PASSWORD} #/" /etc/mysql/debian.cnf
+	mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_USER_PASSWORD}'"
 	service		mysql stop
 else
 	echo "No need to create database, the old one was retrieved"
--- trinception/srcs/requirements/wordpress/wordpress.sh
+++ trinception/srcs/requirements/wordpress/wordpress.sh
@@ -2,15 +2,15 @@
 if [ ! -f "/var/www/wordpress/wp-config.php" ]
 then
 	sleep 15
-	while ! mysql -h mariadb --user=${MARIADB_USR} --password=${MARIADB_USR_PWD} -e "SELECT schema_name FROM information_schema.schemata WHERE schema_name='$MARIADB_NAME'"; do
+	while ! mysql -h mariadb --user=${DB_USER} --password=${DB_USER_PASSWORD} -e "SELECT schema_name FROM information_schema.schemata WHERE schema_name='$DB_NAME'"; do
   		sleep 5
 	done
 
 	echo "Starting installation wordpress"
 	wp core download --allow-root
-	wp config create --allow-root --dbname=${MARIADB_NAME} --dbuser=${MARIADB_USR} --dbpass=${MARIADB_USR_PWD} --dbhost=${MARIADB_HOST} --force
-	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USR} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL}
-	wp user create --allow-root ${WP_TEST_USR} ${WP_TEST_EMAIL} --user_pass=${WP_TEST_PWD} --role=subscriber
+	wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_USER_PASSWORD} --dbhost=${DB_HOST} --force
+	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+	wp user create --allow-root ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=subscriber
 	wp theme install riverbank --allow-root --activate
 
 else
