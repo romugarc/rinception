ab76d4efc6fc09fb0a19bdf64d8441d3293b3bf1
rgarcia <rgarcia@debian.saucisse.42.fr>
2023-09-21 08:57:56 -0400

    ligne password oblige

--- binception/srcs/requirements/mariadb/tools/db_script.sh
+++ binception/srcs/requirements/mariadb/tools/db_script.sh
@@ -8,8 +8,8 @@ then
 	mysql -e	"CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
 	mysql -e	"GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}' WITH GRANT OPTION;"
 	mysql -e	"FLUSH PRIVILEGES;"
-	sed -i		"s/password = /password = ${DB_USER_PASSWORD} #/" /etc/mysql/debian.cnf
-	mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_USER_PASSWORD}'"
+	sed -i		"s/password =/password = ${DB_USER_PASSWORD} #/" /etc/mysql/debian.cnf
+	mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_USER_PASSWORD}';"
 	service		mysql stop
 else
 	echo "No need to create database, the old one was retrieved"
